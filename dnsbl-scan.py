4e7fafa1be3816ab427e7cd98e1c94adea559e38
John Cox <jc@yidhra.outer.uphall.net>
2015-09-19 18:09:25 +0100

    Add BL type to cache & fix double reverse

--- dnsbl-scan.py
+++ dnsbl-scan.py
@@ -13,12 +13,13 @@ new_state = {}
 class CacheRecord:
     expire_delta = 24 * 60 * 60
 
-    def __init__ (self, state, now):
+    def __init__ (self, state, now, blacktype):
         self.state = state
         self.time = now
+        self.blacktype = int(blacktype)
 
     def file_str (self, ip):
-        return self.state + "|" + ip + "|" + str(self.time)
+        return self.state + "|" + ip + "|" + str(self.time) + "|" + str(self.blacktype)
 
     def expired (self):
         return now < self.time + self.expire_delta
@@ -50,7 +51,6 @@ def spam_lookup (ip, cur_type, record="A"):
         print "### Bad IP4 ", ip
         return
 
-    saddr.reverse()
     rip = saddr[3] + "." + saddr[2] + "." + saddr[1] + "." + saddr[0] + "." + "zen.spamhaus.org"
 
     print ">>> [", cur_type, "] ", rip
@@ -58,10 +58,10 @@ def spam_lookup (ip, cur_type, record="A"):
     dig_out = subprocess.check_output(["dig", "+short", record, rip])
     for a_rec in dig_out.splitlines():
         if a_rec[0:8] == "127.0.0.":
-            new_state[ip] = CacheRecord("BLACK", now)
+            new_state[ip] = CacheRecord("BLACK", now, a_rec[8:])
             break
     else:
-        new_state[ip] = CacheRecord("GREY", now)
+        new_state[ip] = CacheRecord("GREY", now, "0")
 
 
 if __name__ == "__main__":
@@ -81,8 +81,8 @@ if __name__ == "__main__":
         for line in fin :
             parts = line.split("|")
             cache_time = int(parts[2])
-            if now < cache_time + CacheRecord.expire_delta:
-                cached_state[parts[1]] = CacheRecord(parts[0], cache_time)
+            if len(parts) >= 4 and now < cache_time + CacheRecord.expire_delta:
+                cached_state[parts[1]] = CacheRecord(parts[0], cache_time, parts[3])
     except IOError:
         pass  # Probably doesn't exist
 
