f92dab3d3972e0734bd51c1cafd7899be43007e9
John Cox <jc@yidhra.outer.uphall.net>
2015-09-19 19:25:29 +0100

    Add spamdb interaction

--- dnsbl-scan.py
+++ dnsbl-scan.py
@@ -9,6 +9,7 @@ from optparse import OptionParser
 
 cached_state = {}
 new_state = {}
+do_trap = True
 
 class CacheRecord:
     expire_delta = 24 * 60 * 60
@@ -25,51 +26,69 @@ class CacheRecord:
         return now < self.time + self.expire_delta
 
 def do_black (ip, cur_type, black_type):
-    print "### BLACK", black_type
+    global do_trap
+
+    if not do_trap:
+        print "### BLACK (", black_type, ")", ip
+    else:
+        subprocess.check_call(["spamdb", "-a", "-t", ip])
     return
 
 def spam_lookup (ip, cur_type, record="A"):
     global cached_state
     global new_state
 
-    now = int(time.time())
-
-    if ip in cached_state:
-        print "### Got cached state", cached_state[ip]
-        new_state[ip] = cached_state[ip]
+    if cur_type != "WHITE" and cur_type != "GREY":
         return
 
+    # If already in new_state then we have already done anything required
     if ip in new_state:
         print "### Got new state", new_state[ip]
         return
 
-    if cur_type != "WHITE" and cur_type != "GREY":
-        return
+    now = int(time.time())
 
-    saddr = ip.split(".")
-    if len(saddr) != 4:
-        print "### Bad IP4 ", ip
-        return
-
-    rip = saddr[3] + "." + saddr[2] + "." + saddr[1] + "." + saddr[0] + "." + "zen.spamhaus.org"
-
-    print ">>> [", cur_type, "] ", rip
-
-    dig_out = subprocess.check_output(["dig", "+short", record, rip])
-    for a_rec in dig_out.splitlines():
-        if a_rec[0:8] == "127.0.0.":
-            new_state[ip] = CacheRecord("BLACK", now, a_rec[8:])
-            break
+    if ip in cached_state:
+        this_state = cached_state[ip]
+        print "### Got cached state", this_state.file_str(ip)
     else:
-        new_state[ip] = CacheRecord("GREY", now, "0")
+        # Need new lookup
+        saddr = ip.split(".")
+        if len(saddr) != 4:
+            print "### Bad IP4 ", ip
+            return
+
+        rip = saddr[3] + "." + saddr[2] + "." + saddr[1] + "." + saddr[0] + "." + "zen.spamhaus.org"
+
+        print ">>> [", cur_type, "] ", rip
+
+        dig_out = subprocess.check_output(["dig", "+short", record, rip])
+        black_type = 256
+        for a_rec in dig_out.splitlines():
+            if a_rec[0:8] == "127.0.0.":
+                this_black = int(a_rec[8:])
+                if this_black != 0 and this_black < black_type:
+                    black_type = this_black;
+
+        if black_type != 256:
+            this_state = CacheRecord("BLACK", now, black_type)
+        else:
+            this_state = CacheRecord("GREY", now, 0)
+
+    if this_state.state == "BLACK":
+        do_black(ip, cur_type, this_state.blacktype)
+
+    new_state[ip] = this_state
 
 
 if __name__ == "__main__":
     parser = OptionParser()
     parser.add_option("-f", "--file", action="store", type="string", dest="filename")
+    parser.add_option("-n", "--no-trap", action="store_true", dest="no_trap")
     (opts, args) = parser.parse_args(sys.argv[1:])
 
     print "#<<<", opts, args
+    do_trap = not opts.no_trap
 
     cache_name = "dnsbl-scan.cache"
     cache_temp = cache_name + ".$$$"
@@ -89,9 +108,13 @@ if __name__ == "__main__":
     fin.close()
 
     if opts.filename:
-        for line in fileinput.input(opts.filename) :
-            parts = line.split("|")
-            spam_lookup(parts[1], parts[0])
+        dbsrc = fileinput.input(opts.filename)
+    else:
+        dbsrc = subprocess.check_output(["spamdb"])
+
+    for line in dbsrc:
+        parts = line.split("|")
+        spam_lookup(parts[1], parts[0])
 
     fout = open(cache_temp, "w")
     for ip in new_state:
